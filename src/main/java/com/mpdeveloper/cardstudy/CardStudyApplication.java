1a7cb7b98ead2a766a72a375466f248c31bf58c4
Marcos Paulo <marcos.p.mcruz@gmail.com>
2022-07-01 21:50:40 -0300

    Projeto iniciado
    Iniciando o projeto spring

---
+++ src/main/java/com/mpdeveloper/cardstudy/CardStudyApplication.java
@@ -0,0 +1,13 @@
+package com.mpdeveloper.cardstudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CardStudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CardStudyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mpdeveloper/cardstudy/CardStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mpdeveloper.cardstudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CardStudyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
